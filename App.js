21bba22b0f58e0e81e9c717bdb117764e7ac4429
Prat2103 <62280674+Prat2103@users.noreply.github.com>
2020-03-30 14:44:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import Youtube from '../APIs/Youtube';
+
+class App extends React.Component
+{
+    
+    onTermSubmit = (term) => {
+        Youtube.get('/search',{
+            params: {
+                q: term
+            }
+        });
+
+    };
+
+
+    render = () => {
+        return (
+            <div className = "ui container">
+                <SearchBar onTermSubmit = {this.onTermSubmit}/>
+            </div>
+        );
+    }
+}
+
+export default App;
